ae0f19d4b8b585603b8599e9466c9416d77210c5
Yuriy Syrovetskiy <cblp@cblp.su>
2017-06-14 14:58:54 +0300

    Fix search_impl; Add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -29,20 +29,18 @@ struct Match {
 fn search_impl<T>(lines: T, needle: &str) -> Vec<Match>
     where T: Iterator<Item = String>
 {
-    let mut matches = Vec::new();
-    for (i, line) in lines.enumerate() {
-        if line.contains(needle) {
-            matches.push(Match { line: i + 1 });
-            println!("{} found @ line {}", line, i + 1);
-        }
-    }
-    Vec::new()
+    lines
+    .enumerate()
+    .filter_map(|(i, line)|
+        if line.contains(needle) {Some(Match{line: i + 1})} else {None}
+    )
+    .collect()
 }
 
 pub fn search(haystack: &str, needle: &str) {
     let file = File::open(haystack).expect("File not found");
     let reader = BufReader::new(file);
-    let lines = reader.lines().filter_map(|s| { s.ok() } );
+    let lines = reader.lines().filter_map(Result::ok);
     search_impl(lines, needle);
 }
 
@@ -55,4 +53,31 @@ fn search_works() {
     ];
     let matches = search_impl(lines.into_iter(), "bar");
     assert_eq!(matches[0], Match { line: 2 });
+}
+
+#[test]
+fn search_empty() {
+    assert_eq!(search_impl(vec![].into_iter(), "bar"), []);
+}
+
+#[test]
+fn search_return_many_cyr() {
+    let lines = vec![
+        String::from("фу"),
+        String::from("бар"),
+        String::from("баз")
+    ];
+    let matches = search_impl(lines.into_iter(), "ба");
+    assert_eq!(matches.len(), 2);
+}
+
+#[test]
+fn search_none_from_some() {
+    let lines = vec![
+        String::from("foo"),
+        String::from("bar"),
+        String::from("baz")
+    ];
+    let matches = search_impl(lines.into_iter(), "qux");
+    assert_eq!(matches, []);
 }
